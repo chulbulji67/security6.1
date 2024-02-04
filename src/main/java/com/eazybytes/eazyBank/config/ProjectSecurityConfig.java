67329e8d07640ff43594ca57601b914d1a606a91
Chulbul <chulbulji67@gmail.com>
2024-02-04 13:05:00 +0530

    endpoints accessible using customer table credential

---
+++ src/main/java/com/eazybytes/eazyBank/config/ProjectSecurityConfig.java
@@ -0,0 +1,84 @@
+package com.eazybytes.eazyBank.config;
+
+
+import com.eazybytes.eazyBank.controller.NoticesController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    //This method is used for the securing the endpoints
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        /**
+         *  Below is the custom security configurations
+         */
+
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/notices","/contact").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+
+        /**
+         *  Configuration to deny all the requests
+         */
+        /*http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();*/
+
+        /**
+         *  Configuration to permit all the requests
+         */
+        /*http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();*/
+    }
+
+
+    //Trying to create Inmemory user so they can access the endpoints
+    /*Approach 1 where we use withDefaultPasswordEncoder() mehtod*/
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService(){
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("password")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .authorities("user")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin,user);
+
+        //Approch 2 using NoOpPasswordEncoder
+//        UserDetails admin = User.withUsername("admin")
+//                .password("password")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withUsername("user")
+//                .password("password")
+//                .authorities("user")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin,user);
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/java/com/eazybytes/eazyBank/ripository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.eazybytes.eazyBank.ripository;
+
+import com.eazybytes.eazyBank.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    List<Customer> findByEmail(String email);
+}
